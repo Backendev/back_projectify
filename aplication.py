66285963107d1750a2939796be71c34c306b58fa
Backender <backender.dev@gmail.com>
2022-03-28 15:21:36 -0500

    add heroku deploy files

--- aplication.py
+++ aplication.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 from router import Routers
-
+import os
 
 class Aplication:
     def __init__(self,port):
         self.app = Flask(__name__)
-        self.port = port
+        self.port = os.environ.get('PORT', 5000)
         self.router()
     def router(self):
         Routers(self.app)
