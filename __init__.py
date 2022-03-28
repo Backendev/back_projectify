d0d38c123e1315a0c269dcb9930233bb8b5db1e1
Backender <backender.dev@gmail.com>
2022-03-28 15:05:11 -0500

    add heroku deploy files

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ class Endpoint:
         self.parser = argparse.ArgumentParser(description='Servir aplicacion')
         self.parser.add_argument('-p', '--port', type=int, help='Port aplication server')
         self.args = self.parser.parse_args()
-        self.port = 3000
+        self.port = 8000
         if self.args.port != None:
             self.port = self.args.port
         a = Aplication(port=self.port)
